b5b76cc2bcbd0f90b92b4b58aa769325d8b6c220
Robin Scholle <r.scholle@fkf.mpg.de>
2023-02-20 10:02:16 +0100

    Update RealSpace_HartreeFock_HubbardModel.py

--- RealSpace_HartreeFock_HubbardModel.py
+++ RealSpace_HartreeFock_HubbardModel.py
@@ -1,6 +1,6 @@
 #We encourage readers to try out the code and would be genuinely glad, if our code can be used for different projects
 #or extensions of this current one. Note that you need at least python 3 to properly run the code. Please remember to
-#cite this paper, if the code helped produce interesting results.
+#cite our paper, if the code helped produce interesting results.
 
 import numpy as np
 import scipy as sp
